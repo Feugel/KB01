51b58532324bb50960915b18b7979719baaaa855
LTetteroo <lars_tetteroo@hotmail.com>
2014-01-17 12:53:27 +0100

    Inputmouse aangepast.

--- Engine/Engine/InputMouse.h
+++ Engine/Engine/InputMouse.h
@@ -1,24 +1,33 @@
-#ifndef INPUTMOUSE_H
-#define INPUTMOUSE_H
-
-#include "WinDef.h"
-
-
-class InputMouse
-{
-public:
-	InputMouse(HWND);
-	bool InitMouse();
-	bool DoAcquire();
-	void SetTheMouseBuffer();
-	void ResetMouseStruct()
-	MouseStruct GetMouseInput();
-	virtual ~InputMouse(void);
-	void Cleanup();
-
-private:
-	
-	
-}
-
-#endif
+//#ifndef INPUTMOUSE_H
+//#define INPUTMOUSE_H
+//
+//#include "WinDef.h"
+//
+//
+//class InputMouse
+//{
+//public:
+//	InputMouse(HWND);
+//	bool InitMouse();
+//	bool DoAcquire();
+//	void SetTheMouseBuffer();
+//	void ResetMouseStruct();
+//	void SaveReleaseDevice();
+//	MouseStruct GetMouseInput();
+//	virtual ~InputMouse(void);
+//	void Cleanup();
+//	MouseStruct struct MouseStruct
+//	{
+//		int mouse_x;
+//		int mouse_y;
+//	};
+//
+//private:
+//	DIPROPDWORD dipdw;
+//	HWND hwnd;
+//	
+//	LPDIRECTINPUT8 dInput;
+//	LPDIRECTINPUTDEVICE8 dDevice;
+//}
+//
+//#endif
