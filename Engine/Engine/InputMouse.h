b0639d111e85b92f6b5040628712c9a3fa19f75c
LTetteroo <lars_tetteroo@hotmail.com>
2014-01-17 12:46:23 +0100

    InputMouse.ccp en header aangepast.

--- Engine/Engine/InputMouse.h
+++ Engine/Engine/InputMouse.h
@@ -1,22 +1,24 @@
-//#ifndef INPUTMOUSE_H
-//#define INPUTMOUSE_H
-//
-//#include "WinDef.h"
-//
-//
-//class InputMouse
-//{
-//public:
-//	InputMouse(HWND);
-//	virtual ~InputMouse(void);
-//	void Cleanup();
-//
-//private:
-//	bool InitMouse();
-//	MouseStruct GetMouseInput();
-//	bool DoAcquire();
-//	void SetTheMouseBuffer();
-//	void ResetMouseStruct()
-//}
-//
-//#endif
+#ifndef INPUTMOUSE_H
+#define INPUTMOUSE_H
+
+#include "WinDef.h"
+
+
+class InputMouse
+{
+public:
+	InputMouse(HWND);
+	bool InitMouse();
+	bool DoAcquire();
+	void SetTheMouseBuffer();
+	void ResetMouseStruct()
+	MouseStruct GetMouseInput();
+	virtual ~InputMouse(void);
+	void Cleanup();
+
+private:
+	
+	
+}
+
+#endif
