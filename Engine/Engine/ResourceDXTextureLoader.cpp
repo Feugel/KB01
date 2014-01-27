bf198e3bd9dd503b9c6303ea85e40717ccc2f2e0
Feugel <fogel_alphen@hotmail.com>
2014-01-27 15:12:52 +0100

    Added check to GetTexture

--- Engine/Engine/ResourceDXTextureLoader.cpp
+++ Engine/Engine/ResourceDXTextureLoader.cpp
@@ -1,4 +1,5 @@
 #include "ResourceDXTextureLoader.h"
+#include "LogManager.h"
 
 ResourceDXTextureLoader::ResourceDXTextureLoader(void)
 {
@@ -25,11 +26,17 @@ void ResourceDXTextureLoader::Cleanup()
 ResourceTexture* ResourceDXTextureLoader::LoadFile(LPCWSTR fileName, VOID* device)
 {
 	LPDIRECT3DTEXTURE9 texture;
-
+	
 	this->d3dDevice = (LPDIRECT3DDEVICE9)device;
 
 	D3DXCreateTextureFromFile(this->d3dDevice, fileName, &texture);
 	
+	if(!texture)
+	{
+		LogManager::Instance()->Log(LogLevel::WARNING, "%s %s", "Could not find texture for filename", fileName);
+		return new ResourceTexture();
+	}
+
 	this->d3dDevice->SetTexture(0, texture);
 
 	this->d3dDevice->SetTextureStageState(0,D3DTSS_COLOROP,D3DTOP_SELECTARG1);
